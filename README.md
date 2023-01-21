b86ff304c3a07aa44ac15fcd538aed44545fbc93
Francisco Alvarez Raineri <FranciscoAlvarezRaineri@gmail.com>
2023-01-21 17:05:03 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Francisco Alvarez Raineri
+
+# Full Stack Developer
+
+# This is my portfolio
+
+# Deploy @
+
+[![image](https://farportfolio.netlify.app/)
+
+## Based on
+
+[![image](https://nextly.web3templates.com/)
---
+++ pages/index.js
@@ -0,0 +1,66 @@
+import Head from "next/head";
+import Hero from "../components/hero";
+import Navbar from "../components/navbar";
+import SectionTitle from "../components/sectionTitle";
+
+import { benefitOne, benefitTwo } from "../components/data";
+import Benefits from "../components/benefits";
+import Footer from "../components/footer";
+import Testimonials from "../components/testimonials";
+import Cta from "../components/cta";
+import Faq from "../components/faq";
+import PopupWidget from "../components/popupWidget";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>FAR Developer Portfolio</title>
+        <meta
+          name="description"
+          content="Francisco Alvarez Raineri - Full Stack Developer"
+        />
+        <link rel="icon" href="/Retrato low.png" />
+      </Head>
+
+      <Navbar />
+      <br></br>
+      <br></br>
+      <br></br>
+      <Hero />
+      <div id="Projects">
+        <SectionTitle title="Projects">
+          Nextly is a free landing page & marketing website template for
+          startups and indie projects. Its built with Next.js & TailwindCSS. And
+          its completely open-source.
+        </SectionTitle>
+        <Benefits data={benefitOne} />
+        <Benefits imgPos="right" data={benefitTwo} />
+      </div>
+      <SectionTitle
+        pretitle="Watch a video"
+        title="Learn how to fullfil your needs"
+      >
+        This section is to highlight a promo or demo video of your product.
+        Analysts says a landing page with video has 3% more conversion rate. So,
+        don't forget to add one. Just like this.
+      </SectionTitle>
+      <SectionTitle
+        pretitle="Testimonials"
+        title="Here's what our customers said"
+      >
+        Testimonails is a great way to increase the brand trust and awareness.
+        Use this section to highlight your popular customers.
+      </SectionTitle>
+      <Testimonials />
+      <SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
+        Answer your customers possible questions here, it will increase the
+        conversion rate as well as support or chat requests.
+      </SectionTitle>
+      <Faq />
+      <Cta />
+      <Footer />
+      <PopupWidget />
+    </>
+  );
+}
